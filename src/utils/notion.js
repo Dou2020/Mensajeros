1e23f6cd38861812a55021817246453b900b188f
Dou <doueduar2018@gmail.com>
2025-01-19 16:41:10 -0600

    add proyect of the landing astro

---
+++ src/utils/notion.js
@@ -0,0 +1,29 @@
+import { Client } from "@notionhq/client";
+process.loadEnvFile();
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+export async function getData() {
+    try {
+        const response = await notion.databases.query({
+            database_id: process.env.NOTION_DATABASE_ID,
+        });
+        const extractedData = response.results.map((page) => {
+            return {
+                //properties: page.properties,
+                nombre: page.properties.Nombre.title[0].plain_text,
+                descripcion: page.properties.Descripcion.rich_text[0].plain_text,
+                Estado: page.properties.Estado.status.name,
+                fecha: page.properties.Fecha.date.start,
+                file: page.properties.Archivo.files[0].file.url,
+            };
+        });
+
+        console.log(extractedData);
+        
+        return extractedData;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
