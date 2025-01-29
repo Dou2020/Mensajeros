d2f613d7f321101cb33243af0d7b92a3c77e4f35
Dou <doueduar2018@gmail.com>
2025-01-28 22:14:25 -0600

    add page celula

--- src/utils/notion.js
+++ src/utils/notion.js
@@ -14,7 +14,7 @@ export async function getData() {
                 nombre: page.properties.Nombre.title[0]?.plain_text,
                 descripcion: page.properties.Descripcion.rich_text[0]?.plain_text,
                 lugar: page.properties.Lugar.rich_text[0]?.plain_text,
-                Estado: page.properties.Estado.status.name,
+                Estado: page.properties.Estado.status?.name,
                 fecha: page.properties.Fecha.date?.start,
                 file: page.properties.Archivo.files[0]?.name,
             };
@@ -27,4 +27,29 @@ export async function getData() {
         console.error(error);
         return [];
     }
+}
+export async function getData2() {
+    try {
+        const response = await notion.databases.query({
+            database_id: process.env.NOTION_DATABASE_ID2,
+        });
+        const extractedData = response.results.map((page) => {
+            return {
+                //properties: page.properties,
+                nombre: page.properties.Nombre.title[0]?.plain_text,
+                dirige: page.properties.Dirige?.rich_text[0]?.plain_text,
+                predica: page.properties.Predica?.rich_text[0]?.plain_text,
+                lugar: page.properties.Lugar.rich_text[0]?.plain_text,
+                file: page.properties.Archivo.files[0]?.name,
+            };
+        });
+
+        console.log(extractedData);
+        
+        return extractedData;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+
 }
