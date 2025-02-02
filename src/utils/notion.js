2e21e77ccdc3bd5e1c0e133e353954626389c75d
Douglas Gómez <doueduar2018@gmail.com>
2025-02-02 17:21:51 -0600

    add celula

--- src/utils/notion.js
+++ src/utils/notion.js
@@ -66,3 +66,35 @@ export async function getData2() {
     }
 
 }
+export async function getData3() {
+    try {
+        const response = await notion.databases.query({
+            database_id: process.env.NOTION_DATABASE_ID3,
+            filter: {
+                property: "Prioridad",
+                status: {
+                    equals: "Realizar",
+                },
+            },
+        });
+        const extractedData = response.results.map((page) => {
+            return {
+                //properties: page.properties,
+                nombre: page.properties.Nombre.title[0]?.plain_text,
+                dirige: page.properties.Dirige?.rich_text[0]?.plain_text,
+                predica: page.properties.Predica?.rich_text[0]?.plain_text,
+                lugar: page.properties.Lugar?.rich_text[0]?.plain_text,
+                descripcion: page.properties.Descripcion?.rich_text[0]?.plain_text,
+                file: page.properties.Archivo.files[0]?.name,
+            };
+        });
+
+        console.log(extractedData);
+        
+        return extractedData;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+
+}
