9410479533008eb37aeddc1a346ab584ea22e855
Dou <doueduar2018@gmail.com>
2026-01-23 10:39:21 -0600

    add page of jovenes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import flowbite from 'flowbite/plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: [
@@ -8,6 +10,6 @@ export default {
 		extend: {},
 	},
 	plugins: [
-		require('flowbite/plugin'),
+		flowbite,
 	],
 }
